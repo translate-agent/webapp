b7dd9c0fe0338e737f595967850bc0b459bb5843
Egija Sprukule <egija.sprukule@expect.digital>
2023-06-22 15:23:47 +0300

    tidy

--- src/app/services/translate.service.ts
+++ src/app/services/translate.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core'
 import { TranslateService } from '@buf/expectdigital_translate-agent.bufbuild_connect-es/translate/v1/translate_connect'
 import {
   CreateServiceRequest,
+  ListMessagesResponse,
   ListServicesResponse,
   Service,
 } from '@buf/expectdigital_translate-agent.bufbuild_es/translate/v1/translate_pb'
@@ -27,7 +28,11 @@ export class TranslateClientService {
 
   createService(serviceName: string): Promise<Service> {
     const request = new CreateServiceRequest({ service: new Service({ name: serviceName }) })
-
     return this.client.createService(request)
+  }
+
+  listMessages(serviceId: string): Promise<ListMessagesResponse> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+    return this.client.listMessages({ serviceId })
   }
 }
